615e214d1514a0fd122b57275128275c2e736d9a
Omid Parsa <omidparsa.1985@gmail.com>
2020-08-19 01:35:45 +0200

    redirecting if user signed in and action type const creating

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Switch, Route} from 'react-router-dom';
+import {Switch, Route, Redirect} from 'react-router-dom';
 import './App.css';
 import { connect } from 'react-redux';
 import { setCurrentUser } from './redux/user/user.actions';
@@ -14,29 +14,25 @@ import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.com
 
 
 class App extends React.Component {
-  
+
   unsubscribeFromAuth = null;
+
   componentDidMount () {
-    const { setCurrentUser } =this.props;
+    const { setCurrentUser } = this.props;
 
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
         const userRef = await createUserPropileDocument(userAuth);
-        
+
         userRef.onSnapshot(snapShot => {
-          setCurrentUser ({
-              id: snapShot.id,
-              ...snapShot.data()
-            });
+          setCurrentUser({
+            id: snapShot.id,
+            ...snapShot.data()
+          });
         });
-        // console.log(this.state);
-        
-        
-      }else {
-        setCurrentUser({userAuth});
-      }
-      
-    })
+      }      
+      setCurrentUser(userAuth);      
+    });
   }
   componentWillUnmount() {
     this.unsubscribeFromAuth();
@@ -49,13 +45,18 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route exact path='/shop' component={ShopPage}/>
-          <Route exact path='/signin' component={SignInAndSignUp} />
+          <Route exact path='/signin' render={
+            () => this.props.currentUser ? <Redirect to='/' /> : <SignInAndSignUp/>
+          } />
         </Switch>
       </div>
     );
   }
 }
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+});
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 });
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -9,27 +9,31 @@ import {ReactComponent as Logo} from '../../assets/crown.svg';
 
 const Header = ({ currentUser }) => (
     <div className='header'>
-        <Link className='logo-container' to='/'>
-            <Logo className='logo'/>
+      <Link className='logo-container' to='/'>
+        <Logo className='logo' />
+      </Link>
+      <div className='options'>
+        <Link className='option' to='/shop'>
+          SHOP
         </Link>
-        <div className='options'>
-            <Link className='option' to='/shop'>
-                SHOP
-            </Link>
-            <Link className='option' to='/contact'>
-                CONTACT
-            </Link>
-            {
-                currentUser ? 
-                <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>
-                :
-                <Link className='option' to='/signin'>SIGN IN</Link>
-            }
-        </div>
+        <Link className='option' to='/shop'>
+          CONTACT
+        </Link>
+        {currentUser ? (
+          <div className='option' onClick={() => auth.signOut()}>
+            SIGN OUT
+          </div>
+        ) : (
+          <Link className='option' to='/signin'>
+            SIGN IN
+          </Link>
+        )}
+      </div>
     </div>
-);
-const mapStateToProps = state => ({
+  );
+  
+  const mapStateToProps = state => ({
     currentUser: state.user.currentUser
-})
-
-export default connect(mapStateToProps)(Header);
+  });
+  
+  export default connect(mapStateToProps)(Header);
